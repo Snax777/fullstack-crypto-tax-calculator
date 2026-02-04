859097df54a1c4564bdec12da9f448e387a2c46d
khaya05 <khayamnyandu5@gmail.com>
2026-02-04 05:54:07 +0200

    refactor import csv logic

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,10 @@
 <?php
 
 use App\Http\Controllers\Api\TransactionController;
+use App\Http\Controllers\Api\TransactionUploadController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('transactions', TransactionController::class);
 
-Route::post('/upload-transactions', [TransactionUploadController::class, 'upload']);
+Route::post('/transactions/upload', [TransactionUploadController::class, 'upload']);
 // Route::post('/transactions', [TransactionUploadController::class, 'transactions']);
